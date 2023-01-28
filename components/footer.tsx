273ea55203dc895c11872a372dcaebff28081452
YouTubeKosen <private.kosen@gmail.com>
2023-01-28 14:18:41 +0900

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,11 @@ import { YouTubeSubscribe } from './youtubeSubscribe'
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-neutral-50 border-t border-neutral-200 mt-5 py-5">
       <Container>
-        <div className="flex flex-col items-center my-5">
+        <div className="flex flex-col items-center mb-5">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-5 lg:pr-4">
-            チャンネル登録によって入学とみなします！
+            チャンネル登録によって入学手続きが完了します！
           </h3>
           <div className="my-5">
           <YouTubeSubscribe />
