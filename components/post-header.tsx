25e356417db849acec8b1bfd2ca18db4ef525e22
YouTubeKosen <private.kosen@gmail.com>
2023-01-28 15:36:44 +0900

    update post page

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -15,19 +15,12 @@ const PostHeader = ({ title, coverImage, date, author }: Props) => {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
+      <div className="flex items-center md:mb-4 text-xl font-bold">
         <Avatar name={author.name} picture={author.picture} />
+        <DateFormatter dateString={date} />
       </div>
-      <div className="mb-8 md:mb-16 sm:mx-0 flex justify-center">
+      <div className="sm:mx-0 flex justify-center mb-5">
         <CoverImage title={title} src={coverImage} />
-      </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
-        <div className="mb-6 text-lg">
-          <DateFormatter dateString={date} />
-        </div>
       </div>
     </>
   )
