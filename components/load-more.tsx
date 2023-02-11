835e7d8a66db345ab4b5c5a5e82479546375c743
YouTubeKosen <private.kosen@gmail.com>
2023-02-11 14:50:44 +0900

    move who_more_btn

--- components/load-more.tsx
+++ components/load-more.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export const LoadMore = ({ onClick }: Props) => {
   return (
-    <div id ='show_more_btn' className='w-[150px] mx-auto'>
-      <button className="w-[150px] bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+    <div className='w-[150px] mx-auto'>
+      <button id ='show_more_btn' className="w-[150px] bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         onClick={onClick}>もっと見る</button>
     </div>
   );
