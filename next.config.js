d0625cdf59b79f8d7044618c7a4290beeb7371cb
YouTubeKosen <private.kosen@gmail.com>
2023-02-11 14:14:24 +0900

    add GTM id

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 let baseUrl = ''
-// if (process.env.GITHUB_ACTIONS) {
+let gtmId = ''
+if (process.env.GITHUB_ACTIONS) {
 //   baseUrl = '/' + process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-// }
+  gtmId = 'GTM-TCF2V4D'
+}
 
 module.exports = {
-  publicRuntimeConfig: { baseUrl }
+  publicRuntimeConfig: { baseUrl },
+  env: {
+    GTM_ID: gtmId,
+  },
 }
